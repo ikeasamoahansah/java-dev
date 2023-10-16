58dbd5b1c2663acfe5097eab8e745c0608ce8877
Ike <dylanraspino264@gmail.com>
2023-10-16 13:14:39 +0000

    commit

---
+++ pytha.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class pytha {
+
+    public static void main(String args[]) {
+        double x;
+        double y;
+        double z;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a value for x:");
+        x = scanner.nextDouble();
+
+        System.out.println("Enter a value for y:");
+        y = scanner.nextDouble();
+
+        z = Math.sqrt((x*x) + (y*y));
+        System.out.println("The answer is: "+z);
+
+        scanner.close();
+    }
+}
