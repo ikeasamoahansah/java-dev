cf9fb99dbc52fc0268fc287195e1695b025c8826
Ike <dylanraspino264@gmail.com>
2023-10-30 15:59:43 +0000

    commit --day7

---
+++ GUI/myProgressBar.java
@@ -0,0 +1,42 @@
+package GUI;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class myProgressBar {
+
+    JFrame frame = new JFrame();
+    JProgressBar bar = new JProgressBar();
+
+    myProgressBar(){
+
+        bar.setValue(0);
+        bar.setBounds(0,0,420,50);
+        bar.setStringPainted(true);
+        bar.setFont(new Font("MV Boli", Font.BOLD, 25));
+        bar.setBackground(Color.black);
+        bar.setForeground(Color.green);
+
+        frame.add(bar);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(420, 420);
+        frame.setVisible(true);
+
+        fill();
+    }
+
+    public void fill() {
+        int counter = 0;
+        while(counter<=100){
+            bar.setValue(counter);
+            try{
+                Thread.sleep(50);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            counter += 1;
+        }
+        bar.setString("Done! :)");
+    }
+}
---
+++ GUI/progressBar.java
@@ -0,0 +1,8 @@
+package GUI;
+
+
+public class progressBar{
+    public static void main(String[] args) {
+        new myProgressBar();
+    }
+}
